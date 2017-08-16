43f1f613888c9750d67c1b9fdaa17a601bddc912
Tazzai <tazz@barkbox.com>
2017-08-16 09:26:33 -0400

    Created javascript code from scratch, added html, yet to add CSS

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
+$(document).ready(function(){
+  $('#failure').hide();
+  $('#success').hide();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $('button').click(function() {
+    var user = $('input').val();
+    if (oldEnough (user)) {
+      $('#failure').hide();
+      $('#success').show();
+      revealImage();
+    } else {
+      $('#success').hide();
+      $('#failure').show();
+    }
+  });
+});
 
 
 /* DO NOT MODIFY*/
+function oldEnough(age){
+  if (age >= 21) {
+    return true;
+  }
+  else {
+    return false;
+  }
+  }
+
 function revealImage(){
   $("img").attr("src","./img/encrypted.jpg");
 }
